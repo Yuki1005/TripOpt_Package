72bfef974d10503adcb4b02ec8b113f6b7f4e605
Yuki1005 <90876725+Yuki1005@users.noreply.github.com>
2024-01-26 09:13:57 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="TripOpt",
-    version="0.2.0",
+    version="1.0.0",
     author="Yuki1005",
     license='MIT',
     description="You can receive AWS Service Name.",
